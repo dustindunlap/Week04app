b88a2991562032419a60c8f4114d16f7935aae9f
dus <111546791+dustindunlap@users.noreply.github.com>
2022-09-13 18:10:41 +0000

    single json file retrieved. app functioning.

--- week-04-app/pages/[id].js
+++ week-04-app/pages/[id].js
@@ -1,5 +1,5 @@
 import Head from 'next/head';
-import {getAllIds, getData} from '../lib/data.js';
+import { getAllIds, getData } from '../lib/data';
 
 export async function getStaticProps({ params }) {
   const itemData = await getData(params.id);
@@ -20,13 +20,13 @@ export async function getStaticPaths() {
 
 export default function Entry({ itemData }) {
   return (
-  <article class="card col-6">
-    <div class="card-body">
-      <h5 class="card-title">{itemData.name}</h5>
-      <h6 class="card-subtitle mb-2 text-muted">{itemData.phone}</h6>
-      <p class="card text">{itemData.birthday}</p>
-      <a href={'mailto:' + itemData.email} class="card-link">{itemData.email}</a>
-    </div>
-  </article>
-    );
+    <article className="card col-6 text-dark">
+      <div className="card-body">
+        <h5 className="card-title">{itemData.name}</h5>
+        <h6 className="card-subtitle mb-2 text-muted">{itemData.phone}</h6>
+        <p className="card-text">{itemData.birthdate}</p>
+        <a href={'mailto:' + itemData.email} className="card-link">{itemData.email}</a>
+      </div>
+    </article>
+  );
 }
